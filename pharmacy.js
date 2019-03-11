3fb399cb2597f4324ad49d2419285470cd37b396
KitCat <fdelencl@student.42.fr>
2019-03-11 20:51:25 +0100

    refactoring of the Pharmacy and addition of the Dafalgan case

--- pharmacy.js
+++ pharmacy.js
@@ -1,3 +1,5 @@
+// Note: as the rules say a benefit can not be over 50,
+// why not implement a rule on the constructor to enforce that rule during creation?
 export class Drug {
   constructor(name, expiresIn, benefit) {
     this.name = name;
@@ -6,61 +8,53 @@ export class Drug {
   }
 }
 
+const updateExpiresIn = {
+  "Magic Pill": (_, expiresIn) => expiresIn,
+  default: (_, expiresIn) => expiresIn - 1
+};
+
+const updateBenefit = {
+  Dafalgan: (benefit, expiresIn) => {
+    if (expiresIn <= 0) benefit -= 4;
+    else benefit -= 2;
+    return benefit > 0 ? benefit : 0;
+  },
+  Fervex: (benefit, expiresIn) => {
+    if (expiresIn < 6) benefit += 3;
+    else if (expiresIn < 11) benefit += 2;
+    else benefit += 1;
+    if (expiresIn <= 0) benefit = 0;
+    return benefit <= 50 ? benefit : 50;
+  },
+  "Herbal Tea": (benefit, expiresIn) => {
+    if (expiresIn <= 0) benefit += 2;
+    else benefit += 1;
+    return benefit <= 50 ? benefit : 50;
+  },
+  "Magic Pill": benefit => benefit,
+  default: (benefit, expiresIn) => {
+    if (expiresIn <= 0) benefit -= 2;
+    else benefit -= 1;
+    return benefit > 0 ? benefit : 0;
+  }
+};
+
 export class Pharmacy {
   constructor(drugs = []) {
     this.drugs = drugs;
   }
   updateBenefitValue() {
-    for (var i = 0; i < this.drugs.length; i++) {
-      if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
-      ) {
-        if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
-            this.drugs[i].benefit = this.drugs[i].benefit - 1;
-          }
-        }
-      } else {
-        if (this.drugs[i].benefit < 50) {
-          this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
-            if (this.drugs[i].expiresIn < 11) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-            if (this.drugs[i].expiresIn < 6) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.drugs[i].name != "Magic Pill") {
-        this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
-      }
-      if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
-            if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
-                this.drugs[i].benefit = this.drugs[i].benefit - 1;
-              }
-            }
-          } else {
-            this.drugs[i].benefit =
-              this.drugs[i].benefit - this.drugs[i].benefit;
-          }
-        } else {
-          if (this.drugs[i].benefit < 50) {
-            this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          }
-        }
-      }
-    }
-
+    this.drugs.forEach(drug => {
+      const { name, benefit, expiresIn } = drug;
+      drug.expiresIn = (updateExpiresIn[name] || updateExpiresIn.default)(
+        benefit,
+        expiresIn
+      );
+      drug.benefit = (updateBenefit[name] || updateBenefit.default)(
+        benefit,
+        expiresIn
+      );
+    });
     return this.drugs;
   }
 }
--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,83 @@
 import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
+  it("Drugs should decrease the benefit and expiresIn", () => {
     expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
       [new Drug("test", 1, 2)]
     );
+  });
+
+  it("Drugs should decrease in benefit twice as fast once it's expired", () => {
+    expect(
+      new Pharmacy([new Drug("test", 0, 20)]).updateBenefitValue()
+    ).toEqual([new Drug("test", -1, 18)]);
+  });
+
+  it("Benefits should not go under 0", () => {
+    expect(
+      new Pharmacy([
+        new Drug("test", 10, 0),
+        new Drug("Dafalgan", 10, 0),
+        new Drug("Fervex", 0, 10)
+      ]).updateBenefitValue()
+    ).toEqual([
+      new Drug("test", 9, 0),
+      new Drug("Dafalgan", 9, 0),
+      new Drug("Fervex", -1, 0)
+    ]);
+  });
+
+  it("Benefits should increase the right amount for Herbal Tea", () => {
+    expect(
+      new Pharmacy([
+        new Drug("Herbal Tea", 10, 10),
+        new Drug("Herbal Tea", 0, 10)
+      ]).updateBenefitValue()
+    ).toEqual([new Drug("Herbal Tea", 9, 11), new Drug("Herbal Tea", -1, 12)]);
+  });
+
+  it("Benefits should increase the right amount for Fervex", () => {
+    expect(
+      new Pharmacy([
+        new Drug("Fervex", 15, 20),
+        new Drug("Fervex", 10, 20),
+        new Drug("Fervex", 5, 20)
+      ]).updateBenefitValue()
+    ).toEqual([
+      new Drug("Fervex", 14, 21),
+      new Drug("Fervex", 9, 22),
+      new Drug("Fervex", 4, 23)
+    ]);
+  });
+
+  it("Benefits should decrease twice as fast for Dafalgan", () => {
+    expect(
+      new Pharmacy([
+        new Drug("Dafalgan", 10, 10),
+        new Drug("Dafalgan", 0, 10)
+      ]).updateBenefitValue()
+    ).toEqual([new Drug("Dafalgan", 9, 8), new Drug("Dafalgan", -1, 6)]);
+  });
+
+  it("Benefits should not go over 50", () => {
+    expect(
+      new Pharmacy([
+        new Drug("Herbal Tea", 10, 49),
+        new Drug("Herbal Tea", 0, 49),
+        new Drug("Fervex", 10, 49),
+        new Drug("Fervex", 5, 49)
+      ]).updateBenefitValue()
+    ).toEqual([
+      new Drug("Herbal Tea", 9, 50),
+      new Drug("Herbal Tea", -1, 50),
+      new Drug("Fervex", 9, 50),
+      new Drug("Fervex", 4, 50)
+    ]);
+  });
+
+  it("Magic Pill never expires nor decreases in Benefit.", () => {
+    expect(
+      new Pharmacy([new Drug("Magic Pill", 9, 40)]).updateBenefitValue()
+    ).toEqual([new Drug("Magic Pill", 9, 40)]);
   });
 });
